8097e9ea9cbc9e8ab9d6f4ca43a6231298adc03b
R-abodyak <rawanabodiak12@gmail.com>
2022-06-04 15:59:07 +0300

    sign up : modify auto mapper

--- RealWord.DB/Models/Outer Dtos/RegisterOuterDto.cs
+++ RealWord.DB/Models/Outer Dtos/RegisterOuterDto.cs
@@ -2,11 +2,14 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.Json.Serialization;
 
 namespace RealWord.DB.Models
 {
     public class RegisterOuterDto
     {
+        [JsonPropertyName("user")]
+
         public ApplicationUser applicationUser { get; set; }
         // public  UserForRegisterDto  userForRegisterDto { get; set; }
     }
