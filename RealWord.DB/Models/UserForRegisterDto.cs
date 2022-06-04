15bdddcc94d4d8c4cf75e430199e908c17a19b83
R-abodyak <rawanabodiak12@gmail.com>
2022-06-04 12:18:02 +0300

    sign up : add json mapping

--- RealWord.DB/Models/UserForRegisterDto.cs
+++ RealWord.DB/Models/UserForRegisterDto.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.Json.Serialization;
 
 namespace RealWord.DB.Models
 {
     public class UserForRegisterDto
     {
-        public string username { get; set; }
-        public string email { get; set; }
-        public string password { get; set; }
+        [JsonPropertyName("username")]
+        public string UserName { get; set; }
+        [JsonPropertyName("email")]
+        public string Email { get; set; }
+        [JsonPropertyName("password")]
+        public string Password { get; set; }
 
     }
 }
