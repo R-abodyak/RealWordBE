a17230c37bb39437f060bb68ab69d9669085f9fd
R-abodyak <rawanabodiak12@gmail.com>
2022-06-05 17:19:36 +0300

    Log out : Itoken manager interface

---
+++ RealWordBE/Authentication/Logout/ITokenManager.cs
@@ -0,0 +1,10 @@
+﻿namespace RealWordBE.Authentication.Logout
+{
+    public interface ITokenManager
+    {
+        void DeactivateAsync(string token);
+        void DeactivateCurrentAsync();
+        bool IsActiveAsync(string token);
+        bool IsCurrentActiveToken();
+    }
+}
