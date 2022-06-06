b4a8b1ffc225042bcf023aec516bb0d5b45dfd1b
R-abodyak <rawanabodiak12@gmail.com>
2022-06-06 21:36:52 +0300

    Validation :email ,url validation

--- RealWord.DB/Models/RequestDtos/UserForUpdateDto.cs
+++ RealWord.DB/Models/RequestDtos/UserForUpdateDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 using System.Text.Json.Serialization;
 
@@ -10,13 +11,13 @@ namespace RealWord.DB.Models.RequestDtos
 
         [JsonPropertyName("username")]
         public string UserName { get; set; }
-
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
         [JsonPropertyName("email")]
         public string Email { get; set; }
 
         [JsonPropertyName("password")]
         public string Password { get; set; }
-
+        [Url]
         [JsonPropertyName("image")]
         public string Image { get; set; }
 
