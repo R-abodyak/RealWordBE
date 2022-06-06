a53b6b723043ea75350a3d85f2d88f41f89b99ae
R-abodyak <rawanabodiak12@gmail.com>
2022-06-06 11:12:03 +0300

    Update user : add userDto for update

---
+++ RealWord.DB/Models/RequestDtos/UserForUpdateDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace RealWord.DB.Models.RequestDtos
+{
+    public class UserForUpdateDto
+    {
+
+        [JsonPropertyName("username")]
+        public string UserName { get; set; }
+
+        [JsonPropertyName("email")]
+        public string Email { get; set; }
+
+        [JsonPropertyName("password")]
+        public string Password { get; set; }
+
+        [JsonPropertyName("image")]
+        public string Image { get; set; }
+
+        [JsonPropertyName("bio")]
+        public string Bio { get; set; }
+
+    }
+}
