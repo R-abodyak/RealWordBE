9b7a9ae9d484afa76ae19371fc715362f609c251
R-abodyak <rawanabodiak12@gmail.com>
2022-06-06 23:22:44 +0300

    Schema : add main entities

---
+++ RealWord.DB/Entities/Comment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RealWord.DB.Entities
+{
+    public class Comment
+    {
+        public int ArticleId { get; set; }
+
+        public int User_id { get; set; }
+        public string CommentMsg { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+    }
+}
---
+++ RealWord.DB/Entities/Like.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RealWord.DB.Entities
+{
+    public class Like
+    {
+        public int ArticleId { get; set; }
+
+        public int User_id { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
