8f609f41ca85874796c0c968602fd85548ceb0c8
R-abodyak <rawanabodiak12@gmail.com>
2022-06-08 09:27:17 +0300

    Get Profile : passing test for is following logic

--- RealWorld.test/FollowerRepoTest.cs
+++ RealWorld.test/FollowerRepoTest.cs
@@ -32,10 +32,12 @@ namespace RealWorld.test
             User u2 = new User() { Id = "2000" };
             context.Users.Add(u1);
             context.Users.Add(u2);
-            Folower f1 = new Folower() { User = u1 ,follower = u2 };
+            Folower f1 = new Folower() { User = u1 ,UserId = u1.Id ,follower = u2 ,followerId = u2.Id };
             context.Add(f1);
+            context.SaveChanges();
             FollowerRepository followerRepostory = new FollowerRepository(context);
-
+            var result = followerRepostory.IsFollowing(u1.Id ,u2.Id);
+            Assert.True(result);
 
 
 
