fcbbe016306af24fb961e17ec3949d8bdac12743
R-abodyak <rawanabodiak12@gmail.com>
2022-06-10 12:58:43 +0300

    update Article : add updateArticle dto

---
+++ RealWord.DB/Models/RequestDtos/ArticleForUpdateDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RealWord.DB.Models.RequestDtos
+{
+    public class ArticleForUpdateDto
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Body { get; set; }
+    }
+}
