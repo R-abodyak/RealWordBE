2c5fb8c857d55b0470a62d3f6f7fa261dc759679
R-abodyak <rawanabodiak12@gmail.com>
2022-06-11 17:28:51 +0300

    Comment : CommentRebo interface

---
+++ RealWord.DB/Repositories/ICommentRebository.cs
@@ -0,0 +1,15 @@
+﻿using RealWord.DB.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealWord.DB.Repositories
+{
+    public interface ICommentRebository
+    {
+        public Task AddComment(Comment comment);
+        public IEnumerable<Comment> GetComments(string slug);
+        public Task RemoveComment(Comment comment);
+    }
+}
